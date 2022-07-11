f41449cb2ce9e7c665e620f2b028c734ef6d1560
IDontHaveBrain <skawns848@naver.com>
2022-07-11 16:41:25 +0900

    Spring, Mybatis 초기 세팅

---
+++ doc/5차1조 SQL.sql
@@ -0,0 +1,12 @@
+/*
+cmd창 실행
+sqlplus
+system/1111
+CREATE USER p05 IDENTIFIED BY 1111;
+grant dba to p05;
+*/
+/*
+ 테이블, 시퀀스 생성문.
+ 샘플데이터(테이블당 최소 3개 이상씩)
+ 위에서부터 아래로 순차적으로 실행시 모든 테이블 및 샘플데이터 입력되게 작성
+ */
