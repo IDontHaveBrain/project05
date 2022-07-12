58ac8fe541dc2b339ce09f356dd0aca25d0863fc
IDontHaveBrain <skawns848@naver.com>
2022-07-12 16:31:28 +0900

    sql 샘플

--- database/5차1조 SQL.sql
+++ database/5차1조 SQL.sql
@@ -11,6 +11,18 @@ grant dba to p05;
  위에서부터 아래로 순차적으로 실행시 모든 테이블 및 샘플데이터 입력되게 작성
  */
 
+drop table Account;
+-- 계정
+create table Account (
+    idno number primary key,
+    id varchar2(50) not null unique,
+    pw varchar2(50) not null,
+    point number,
+    birthdate DATE
+);
+insert into Account values (1, 'asdasd123', '123123', 50000, to_date('19980101','YYYYMMDD'));
+insert into Account values ((select NVL(max(idno),0)+1 from Account), 'asdasd111', '123456', 30000, to_date('19970505','YYYYMMDD'));
+select * from Account;
 
 -- 홀짝게임결과
 create table OeGameResult (
