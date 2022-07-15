999d97eb1e4e6bb00ea6cf31a6e25c5f54f88b4b
kinjinhan <108657034+kinjinhan@users.noreply.github.com>
2022-07-15 18:32:52 +0900

    공지사항수정

--- src/main/java/toto/vo/Notice.java
+++ src/main/java/toto/vo/Notice.java
@@ -5,7 +5,6 @@ import java.util.Date;
 public class Notice {
 
 	private int no;
-	private int refno;
 	private String division;
 	private String title;
 	private Date regdte;
@@ -13,17 +12,9 @@ public class Notice {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Notice(int no, int refno, String division, String title, Date regdte) {
+	public Notice(int no, String division, String title, Date regdte) {
 		super();
 		this.no = no;
-		this.refno = refno;
-		this.division = division;
-		this.title = title;
-		this.regdte = regdte;
-	}
-	
-	public Notice(String division, String title, Date regdte) {
-		super();
 		this.division = division;
 		this.title = title;
 		this.regdte = regdte;
@@ -33,12 +24,6 @@ public class Notice {
 	}
 	public void setNo(int no) {
 		this.no = no;
-	}
-	public int getRefno() {
-		return refno;
-	}
-	public void setRefno(int refno) {
-		this.refno = refno;
 	}
 	public String getDivision() {
 		return division;
@@ -58,6 +43,11 @@ public class Notice {
 	public void setRegdte(Date regdte) {
 		this.regdte = regdte;
 	}
+	
+	
+	
+	
+	
 	
 	
 	
